fa99391133809e0e4b07c02b1a7d8f85cea5397f
Oleg Vorchuk <olegvorchuk241@gmail.com>
2024-02-08 22:46:24 +0300

    coords and pictures first floor lab

--- bmstu-nav-frontend-main/main.js
+++ bmstu-nav-frontend-main/main.js
@@ -3,6 +3,274 @@ const data = {
         {
             id: 1,
             uuid: "0000-0000-0000-0000-0000",
+            title: "110",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[34, 75], [80, 75], [80, 121], [34, 121]],
+            picture: null
+
+        },
+        {
+            id: 2,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Лифт 1",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[34, 121], [80, 121], [80, 171], [34, 171]],
+            picture: "elevator.svg"
+
+        },
+        {
+            id: 3,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "112",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[80, 75], [196, 75], [196, 171], [80, 171]],
+            picture: null
+
+        },
+        {
+            id: 4,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "112а",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[111, 171], [196, 171], [196, 221], [111, 221]],
+            picture: null
+
+        },
+        {
+            id: 5,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "C106",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[196, 75], [297, 75], [297, 221], [196, 221]],
+            picture: null
+
+        },
+        {
+            id: 6,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "C101",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[297, 75], [392, 75], [392, 221], [297, 221]],
+            picture: null
+
+        },
+        {
+            id: 7,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "C107",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[392, 75], [467, 75], [467, 190], [392, 190]],
+            picture: null
+
+        },
+        {
+            id: 8,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "C108",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[467, 75], [536, 75], [536, 193], [498, 193], [498, 185], [467, 185]],
+            picture: null
+
+        },
+        {
+            id: 9,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "109",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[34, 171], [111, 171], [111, 221], [34, 221]],
+            picture: null
+
+        },
+        {
+            id: 10,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "С103",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[34, 254], [68, 254], [68, 300], [34, 300]],
+            picture: null
+
+        },
+        {
+            id: 11,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "С105",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[68, 254], [98, 254], [98, 300], [68, 300]],
+            picture: null
+
+        },
+        {
+            id: 12,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "С104",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[98, 254], [197, 254], [197, 300], [98, 300]],
+            picture: null
+
+        },
+        {
+            id: 13,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "104",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[197, 254], [300, 254], [300, 300], [197, 300]],
+            picture: "stair.svg"
+
+        },
+        {
+            id: 14,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "111",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[300, 254], [355, 254], [355, 300], [300, 300]],
+            picture: null
+
+        },
+        {
+            id: 15,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "С102",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[355, 254], [421, 254], [421, 300], [355, 300]],
+            picture: null
+
+        },
+        {
+            id: 16,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Туалет",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[421, 233], [467, 233], [467, 263], [421, 263]],
+            picture: "man.svg"
+
+        },
+        {
+            id: 17,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Лестница",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[421, 263], [498, 263], [498, 300], [421, 300]],
+            picture: "stair.svg"
+
+        },
+        {
+            id: 18,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Лифты 2, 3",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[498, 300], [574, 300], [574, 350], [498, 350]],
+            picture: "elevator.svg"
+
+        },
+        {
+            id: 19,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Лестница",
+            isSelected: false,
+            type: "aud",
+            coordinates: [[574, 293], [668, 293], [668, 350], [574, 350]],
+            picture: "stair.svg"
+
+        },
+        {
+            id: 20,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[536, 145], [668, 145], [668, 202], [536, 202]],
+            picture: null
+
+        },
+        {
+            id: 21,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[498, 193], [536, 193], [536, 202], [668, 202], [668, 293], [574, 293],
+                [574, 300], [498, 300]],
+            picture: null
+
+        },
+        {
+            id: 22,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[467, 185], [498, 185], [498, 263], [467, 263]],
+            picture: null
+
+        },
+        {
+            id: 23,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[392, 190], [467, 190], [467, 233], [421, 233], [421, 221], [392, 221]],
+            picture: null
+
+        },
+        {
+            id: 24,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[297, 221], [421, 221], [421, 254], [297, 254]],
+            picture: null
+
+        },
+        {
+            id: 25,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[196, 221], [297, 221], [297, 254], [196, 254]],
+            picture: null
+
+        },
+        {
+            id: 26,
+            uuid: "0000-0000-0000-0000-0000",
+            title: "Коридор",
+            isSelected: false,
+            type: "hall",
+            coordinates: [[34, 221], [196, 221], [196, 254], [34, 254]],
+            picture: null
+
+        },
+    ]
+}
+
+
+
+const data_1 = {
+    objects: [
+        {
+            id: 1,
+            uuid: "0000-0000-0000-0000-0000",
             title: "Аудитория 104???",
             isSelected: false,
             type: "aud",
@@ -1029,4 +1297,40 @@ for (object of data.objects) {
     svg.appendChild(polygon);
     svg.appendChild(pngImage);
 }
+
+// Изменение цвета polygon когда мышка над названием магазина.
+//     $('.first_floor_ulk-item').hover(
+//         function(){
+//             $('.first_floor_ulk polygon[data-id=' + $(this).data('id') + ']').attr('id', 'hover');
+//         },
+//         function(){
+//             $('.first_floor_ulk polygon[data-id=' + $(this).data('id') + ']').attr('id', '');
+//         }
+//     );
+
+    // // Клик по названию магазина - открывается подсказка.
+    // $('.first_floor_ulk-item').on('click', function(){
+    //     $('.first_floor_ulk-popup').hide();
+    //     $('.first_floor_ulk polygon').attr('class', '');
+    //
+    //     var popup = $(this).find('.first_floor_ulk-popup');
+    //     $(popup).css('top', '-' + ($(popup).outerHeight(true) + 15) + 'px');
+    //     $(popup).css('left', '-' + (($(popup).outerWidth(true) / 2) - ($(this).outerWidth(true) / 2)) + 'px');
+    //     $('.first_floor_ulk polygon[data-id=' + $(this).data('id') + ']').attr('class', 'active');
+    //     $(popup).show();
+    // });
+    //
+    // // Клик по полигону магазина - также открывается подсказка.
+    // $('.first_floor_ulk polygon').click(function(){
+    //     $('.first_floor_ulk-popup').hide();
+    //     $('.scheme-item[data-id=' + $(this).data('id') + ']').trigger('click');
+    // });
+    //
+    // // Клик вне магазинов все закрывает.
+    // $("body").click(function(e) {
+    //     if ($(e.target).closest(".first_floor_ulk polygon, .first_floor_ulk-item").length == 0) {
+    //         $(".first_floor_ulk-popup").hide();
+    //         $('.first_floor_ulk polygon').attr('class', '');
+    //     }
+    // });
 
